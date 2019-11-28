40f565b73d48951e84df413e561679e53066b42b
Athensoft <inf.athensoft@hotmail.com>
2019-11-28 14:49:09 -0500

    added py1128

---
+++ sj190912/py1128/project_validator_num.py
@@ -0,0 +1,10 @@
+import re
+def is_number(num):
+    pattern = re.compile(r'^[-+]?[-0-9]\d*\.\d*|[-+]?\.?[0-9]\d*$')
+    result = pattern.match(num)
+    if result:
+        return True
+    else:
+        return False
+
+print(is_number(str(-12.0)))
