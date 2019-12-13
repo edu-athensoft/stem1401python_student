b1559ba95b0c80d972d23795891de5ca9e1b25f8
lada314 <lada314@gmail.com>
2019-12-12 20:39:37 -0500

    added py1205 final project v2

---
+++ sj190912/project_final/demo_func.py
@@ -0,0 +1,21 @@
+def do_sth():
+    print("abc")
+    print("abd")
+    print("abe")
+
+
+do_sth()
+do_sth()
+do_sth()
+
+def double(x):
+    result = 2 * x
+    return result
+
+res = double(2)
+print(res)
+
+
+
+
+
