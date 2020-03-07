176585d57cf2929a3081058a66ccd912d5e9894e
Athensoft <inf.athensoft@hotmail.com>
2020-03-07 09:04:10 -0500

    added project v1

--- sj200116/py200213/print_invoice.py
+++ sj200116/py200213/print_invoice.py
@@ -3,10 +3,10 @@ print out a real invoice
 """
 
 # step 1. to print out document title
-print("East Repair Inc.")
-print("Invoice")
+# print("East Repair Inc.")
+# print("Invoice")
 
-print("East Repair Inc.                                       Invoice")
+# print("East Repair Inc.                                       Invoice")
 
 
 print("{:68s} {:10s}".format("East Repair Inc.","Invoice"))
