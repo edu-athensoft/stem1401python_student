52dbe65733240e2eafe4156dbeaeda2480194337
athensedu <lada314@gmail.com>
2020-06-10 09:02:18 -0400

    added py200609 to py200512

---
+++ py200512/day07_py200609/literal_numeric.py
@@ -0,0 +1,92 @@
+"""
+Numeric literals
+String literals
+Boolean literals
+Special literals
+Literal Collections
+
+Numeric literals
+"""
+
+
+
+
+
+
+"""
+# integers
+-1, -2, -3, ...
+0
+1, 2, 3, 4, ...
+"""
+
+
+"""
+floating number
+1.2
+-2.3
+0.0
+1.20
+1.200
+"""
+
+# case 1.
+d1 = 1
+d2 = 10
+
+# binary
+b1 = 0b1
+b2 = 0b10
+b3 = 0b10110001010101010
+print(b1)
+print(b2)
+print(b3)
+# please write out binary number from 1 to 16
+print("===")
+
+# hex
+h1 = 0xa
+h2 = 0xb
+h3 = 0xc
+h4 = 0xd
+h5 = 0xe
+h6 = 0xf
+print(h1,h2,h3,h4,h5,h6)
+print("===")
+
+# octal
+o1 = 0o1
+o2 = 0O2
+o3 = 0O3
+o4 = 0O4
+o5 = 0O5
+o6 = 0O6
+o7 = 0O7
+o8 = 0O10
+print(o8)
+
+# binary -> hex
+b3 = 0b10110001010101010
+print(hex(b3))
+print(hex(150))
+print("===")
+# hex -> binary
+
+
+# binary -> octal
+b3 = 0b10110001010101010
+print(oct(b3))
+
+# octal -> binary
+o9 = 0O3456
+# 0b11100101110
+print(bin(o9))
+
+
+# decimal -> binary
+d = 547
+print(bin(d))
+
+
+
+
---
+++ py200512/day07_py200609/review_comments.py
@@ -0,0 +1,25 @@
+# this is a single line comment
+
+"""
+docstring
+"""
+
+a = 1
+
+"""
+dataset
+this is a multi-line comment
+"""
+
+
+x, y = 1, 2
+print(x)
+print(y)
+
+# x, y = 2,2
+x = y = 3
+print(x)
+print(y)
+
+
+
